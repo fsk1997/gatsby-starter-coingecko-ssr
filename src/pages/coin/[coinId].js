7d9680d82c8ec129e7af2e72c03667366413d326
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-08 21:11:36 +0000

    feat: add jimp

--- src/pages/coin/[coinId].js
+++ src/pages/coin/[coinId].js
@@ -1,28 +1,30 @@
 import React, { useEffect, useState } from "react"
 import Layout from "../../components/layout"
-import ReactHtmlParser from 'react-html-parser';
+import ReactHtmlParser from "react-html-parser"
+
+import Jimp from "jimp"
 
 const IndexPage = ({ serverData }) => {
-
   const [data, setData] = useState({})
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    setData(serverData)
+    setData(serverData.data)
     setLoading(false)
-    console.log(data)
+    // console.log(data)
   }, [data])
-
 
   return (
     <Layout customSiteTitle={loading ? "loading" : data.id}>
-      {loading ? <p>Loading</p> :
-      <>
-        <h1>{data.name}</h1>
-        <img src={data.image?.large}/>
-        <div>{ReactHtmlParser(data.description.en)}</div>
-      </>
-      }
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <>
+          <h1>{data.name}</h1>
+          <img src={serverData.image} alt={data.name} />
+          <div>{ReactHtmlParser(data.description.en)}</div>
+        </>
+      )}
     </Layout>
   )
 }
@@ -31,22 +33,36 @@ export default IndexPage
 
 export async function getServerData(context) {
   try {
-    const res = await fetch(`https://api.coingecko.com/api/v3/coins/${context.params.coinId}`)
-    // console.log(context.params.coinId)
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/coins/${context.params.coinId}`
+    )
+
+    const data = await res.json()
+
+    let image = ""
+
+    await Jimp.read({ url: data.image.large }).then(img => {
+      img
+        .resize(50, 50)
+        .quality(60)
+        .greyscale()
+        .getBase64(Jimp.AUTO, (err, img) => {
+          image = img
+        })
+    })
 
     if (!res.ok) {
       throw new Error(`Response failed`)
     }
 
     return {
-      props: await res.json()
+      props: { data, image },
     }
-
   } catch (error) {
     return {
       status: 500,
       headers: {},
-      props: {}
+      props: {},
     }
   }
 }
